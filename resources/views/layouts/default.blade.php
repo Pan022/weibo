88ed5fa1d0b9b7d7f8fe1dc0c796d04dc1386b35
Pang K <5038293@qq.com>
2022-07-09 12:41:31 +0000

    user sign-up

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,29 +2,11 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="/css/app.css">
-=======
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 >>>>>>> filling-layout-style
   </head>
 
   <body>
-<<<<<<< HEAD
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
-
-    <div class="container">
-      @yield('content')
-=======
     @include('layouts._header')
 
     <div class="container">
@@ -32,7 +14,6 @@
         @yield('content')
         @include('layouts._footer')
       </div>
->>>>>>> filling-layout-style
     </div>
   </body>
 </html>
