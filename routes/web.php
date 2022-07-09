733fa33b710cfaf75b39ccc86a088cef048df419
Pang K <5038293@qq.com>
2022-07-09 12:52:40 +0000

    user sign-up4

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
+
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
