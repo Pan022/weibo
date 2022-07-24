48abe44820894e985bb387f72e00651998d53712
Pang K <5038293@qq.com>
2022-07-24 15:09:54 +0000

    activated migration succesful

--- database/migrations/2022_07_24_150633_add_activation_to_users_table.php
+++ database/migrations/2022_07_24_150633_add_activation_to_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            //
             $table->string('activation_token')->nullable();
             $table->boolean('activated')->default(false);
-            //
         });
     }
 
